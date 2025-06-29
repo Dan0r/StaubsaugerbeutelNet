1e9c5492628a4b0e7ca7ccdb1de0b7935a321753
furukawa <dkszoeke@outlook.de>
2025-06-29 18:40:20 +0200

    first working version
    First website made with Springboot and Thymeleaf

--- src/main/java/staubsaugerbeutelnet/de/staub/StaubApplication.java
+++ src/main/java/staubsaugerbeutelnet/de/staub/StaubApplication.java
@@ -1,7 +1,13 @@
 package staubsaugerbeutelnet.de.staub;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class StaubApplication {
@@ -10,5 +16,19 @@ public class StaubApplication {
 		SpringApplication.run(StaubApplication.class, args);
 
 	}
+	// Debugging: It basically tells you in the console, which objects Spring has registered.
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
 
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
 }
